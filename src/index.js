a4d82de7046f8534a3d66f787efe93e91fb46236
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-10 15:54:20 +0200

    OOP

--- src/index.js
+++ src/index.js
@@ -3,86 +3,97 @@ import  ApiServer  from "./partials/js/apiService.js";
 import  gallery  from "./partials/templates/gallery.hbs";
 import LoadBtn from "./partials/js/loadBtn.js";
 
-const form = document.querySelector('#search-form');
-const galleryContainer = document.querySelector('.js-container__gallery');
-const newApi = new ApiServer();
-const loadMoreBtn = new LoadBtn({ hidden: true });
+// const form = document.querySelector('#search-form');
+// const galleryContainer = document.querySelector('.js-container__gallery');
+// const newApi = new ApiServer();
+// const loadMoreBtn = new LoadBtn({ hidden: true });
 
 
-form.addEventListener('submit', onSearchForm);
-loadMoreBtn.loadbtn.addEventListener('click', onLoadBtn);
+class WebKit   {
+    constructor() {
+        this.form = document.querySelector('#search-form');
+        this.galleryContainer = document.querySelector('.js-container__gallery');
+        this.newApi = new ApiServer();
+        this.loadMoreBtn = new LoadBtn({ hidden: true });
+    }
 
+    onSearchForm = evt => {
+        evt.preventDefault();
+        this.loadMoreBtn.show();
+        
+        this.clearMarkUp();
+        
+        this.newApi.resetPage();
+        const inputValue = evt.target.elements.query.value.trim();
+        
+        this.newApi.query = inputValue;
+        this.newApi.fetchPrhoto().then(this.pictureMarkUp)
+    }
 
-function onSearchForm(evt) {
-    evt.preventDefault();
-    loadMoreBtn.show();
-    clearMarkUp();
-    newApi.resetPage();
+    pictureMarkUp = picture => {
+        this.galleryContainer.insertAdjacentHTML('beforeend', gallery(picture));
+    }
 
-    const inputValue = evt.target.elements.query.value.trim();
-    newApi.query = inputValue;
-    newApi.fetchPrhoto().then(pictureMarkUp)
-}
+    onLoadBtn = () => {
+        this.newApi.fetchPrhoto().then(this.pictureMarkUp);
+        this.newApi.incrementPage();
+        setTimeout(() => {
+            this.galleryContainer.scrollIntoView({
+                behavior: 'smooth',
+                block: 'end',
+            })
+        }, 1000);
+    }
 
-function pictureMarkUp(picture) {
-    galleryContainer.insertAdjacentHTML('beforeend', gallery(picture));
-}
-function onLoadBtn() {
-    newApi.fetchPrhoto().then(pictureMarkUp);
-    newApi.incrementPage();
-    setTimeout(() => {
-        galleryContainer.scrollIntoView({
-            behavior: 'smooth',
-            block: 'end',
-        })
-    }, 1000);
-}
-
-function clearMarkUp() {
-    galleryContainer.innerHTML = '';
+    clearMarkUp =()=> {
+        this.galleryContainer.innerHTML = '';
+    }
     
+    eventListeners = () => {
+        this.form.addEventListener('submit', this.onSearchForm);
+        this.loadMoreBtn.loadbtn.addEventListener('click', this.onLoadBtn);
+    }
+
+    init = () => {
+        this.eventListeners();
+    }
 }
-    
+
+const web = new WebKit();
+web.init();
 
 
+// form.addEventListener('submit', onSearchForm);
+// loadMoreBtn.loadbtn.addEventListener('click', onLoadBtn);
 
 
-// function onSearchFormSubmit(e){
-//     e.preventDefault();
-//     ApiService.query =  e.currentTarget.elements.query.value;
-    
-//     // loadMoreBtn.show();
-//     // ApiService.resetPage();
-//     clearPhotosContainer();
-//     onLoadMore();
+// function onSearchForm(evt) {
+//     evt.preventDefault();
+//     loadMoreBtn.show();
+//     clearMarkUp();
+//     newApi.resetPage();
+
+//     const inputValue = evt.target.elements.query.value.trim();
+//     newApi.query = inputValue;
+//     newApi.fetchPrhoto().then(pictureMarkUp)
 // }
 
-//  function  onLoadMore(){
-
-//     // loadMoreBtn.disable();
-    
-//     ApiService.fetchPhoto()
-//     .then(photos =>{
-//       const photosListMarkup = photosMap(photos);
-//       galleryContainer.insertAdjacentHTML('beforeend', photosListMarkup);
-//       scrollPage();
-//       loadMoreBtn.enable();
-//     })
- 
+// function pictureMarkUp(picture) {
+//     galleryContainer.insertAdjacentHTML('beforeend', gallery(picture));
+// }
+// function onLoadBtn() {
+//     newApi.fetchPrhoto().then(pictureMarkUp);
+//     newApi.incrementPage();
+//     setTimeout(() => {
+//         galleryContainer.scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'end',
+//         })
+//     }, 1000);
 // }
 
-
-// function clearPhotosContainer() {
+// function clearMarkUp() {
 //     galleryContainer.innerHTML = '';
-//   }
-
-// function photosMap(photos){
-//   return  photos.map(gallery).join('');
+    
 // }
-// function scrollPage(){
-//   window.scrollTo({
-//     top: window.pageYOffset + documentHeight,
-//     width: 0,
-//     behavior: "smooth",
-//   });
-// }
+    
